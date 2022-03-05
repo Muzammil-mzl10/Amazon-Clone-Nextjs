7ab1f0eddf3305af0214cf0ce87f1c62644bbe59
Muzammil Ahmad <56504584+Muzammil786-beep@users.noreply.github.com>
2022-03-05 21:20:05 +0500

    hello

--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,38 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Header from "../components/Header";
-export default function Home() {
+import Banner from "../components/Banner";
+import ProductFeed from "../components/ProductFeed";
+export default function Home(props) {
+  // console.log(props.data);
   return (
     <>
-     <div>
-       <Head>
-         <title>Amazon 2.0</title>
-       </Head>
-      
-    {/* <div className=" flex items-center flex-grow py-2 bg-amazon_blue"></div> */}
+      <div className="bg-gray-100">
+        <Head>
+          <title>Amazon 2.0</title>
+        </Head>
 
-      <Header/>
+        <Header />
 
-     </div>
+        <main className="max-w-screen-2xl mx-auto">
+          <Banner />
+
+          <ProductFeed 
+            products =  {props.data}
+          />
+        </main>
+      </div>
     </>
   );
+}
+// https://fakestoreapi.com/products
+export async function getServerSideProps(context) {
+
+  const products = await fetch('https://fakestoreapi.com/products')
+  const data  = await products.json();
+
+
+  return {
+    props: {data}, 
+  }
 }

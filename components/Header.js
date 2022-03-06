348da0e67f76376e26d498d786e91fa210cb8278
Muzammil Ahmad <56504584+Muzammil786-beep@users.noreply.github.com>
2022-03-07 00:39:21 +0500

    hello

--- components/Header.js
+++ components/Header.js
@@ -7,9 +7,13 @@ import {
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
 
-import { useSession, signIn, signOut } from "next-auth/react"
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const Header = () => {
+
+  const { data: session, status } = useSession()
+  // session.user.
+
   return (
     <>
       <header>
@@ -34,9 +38,14 @@ const Header = () => {
             />
             <SearchIcon className="h-12 p-4" />
           </div>
-          <div onClick={signIn} className="text-white flex items-center text-xs space-x-6 mx-5 whitespace-nowrap">
+          <div
+              onClick={!session ? signIn : signOut}
+            className="text-white flex items-center text-xs space-x-6 mx-5 whitespace-nowrap"
+          >
             <div className="cursor-pointer link">
-              <p>Hello Muzammil Ahmad</p>
+              <p className="hover:underline">
+             {session ? `Hello, ${session.user.name}` : "Sign In"}
+              </p>
               <p className="font-extrabold md:text-sm">Account & List</p>
             </div>
             <div className="cursor-pointer  link">
@@ -44,31 +53,32 @@ const Header = () => {
               <p className="font-extrabold md:text-sm">& Orders</p>
             </div>
             <div className=" relative cursor-pointer link flex items-center">
-
-              <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">0</span>
+              <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
+                0
+              </span>
               <ShoppingCartIcon className="h-10" />
-              <p className="hidden md:inline font-extrabold md:text-sm mt-2">Basket</p>
+              <p className="hidden md:inline font-extrabold md:text-sm mt-2">
+                Basket
+              </p>
             </div>
           </div>
         </div>
 
         <div className="flex items-center bg-amazon_blue-light text-white text-sm space-x-3 p-2 pl-6">
-        
-        <p className="link flex items-center ">
-          <MenuIcon className="h-6 mr-1" />
-          All
-        </p>
-        <p className="link ">Prime Video</p>
-        <p className="link ">Amazon Business</p>
-        <p className="link ">Todays Deals</p>
-        <p className="link hidden lg:inline-flex">Electronics</p>
-        <p className="link hidden lg:inline-flex">Food & Grocery</p>
-        <p className="link hidden lg:inline-flex">Prime</p>
-        <p className="link hidden lg:inline-flex">Buy Apple</p>
-        <p className="link hidden lg:inline-flex">Shopper Toolkit</p>
-        <p className="link hidden lg:inline-flex">Health & Personal Care </p>
-        
-      </div>
+          <p className="link flex items-center ">
+            <MenuIcon className="h-6 mr-1" />
+            All
+          </p>
+          <p className="link ">Prime Video</p>
+          <p className="link ">Amazon Business</p>
+          <p className="link ">Todays Deals</p>
+          <p className="link hidden lg:inline-flex">Electronics</p>
+          <p className="link hidden lg:inline-flex">Food & Grocery</p>
+          <p className="link hidden lg:inline-flex">Prime</p>
+          <p className="link hidden lg:inline-flex">Buy Apple</p>
+          <p className="link hidden lg:inline-flex">Shopper Toolkit</p>
+          <p className="link hidden lg:inline-flex">Health & Personal Care </p>
+        </div>
       </header>
     </>
   );

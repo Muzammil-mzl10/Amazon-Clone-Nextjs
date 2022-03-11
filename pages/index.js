72ffad3ea2449f64592706d7d2f2ef57203b9e49
Muzammil Ahmad <56504584+Muzammil-mzl10@users.noreply.github.com>
2022-03-11 15:01:56 +0500

    dasd

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
+import { getSession } from "next-auth/react";
 import ProductFeed from "../components/ProductFeed";
 export default function Home(props) {
   // console.log(props.data);
@@ -28,12 +29,14 @@ export default function Home(props) {
 }
 // https://fakestoreapi.com/products
 export async function getServerSideProps(context) {
-
+  const session = await getSession(context)
   const products = await fetch('https://fakestoreapi.com/products')
   const data  = await products.json();
 
 
   return {
-    props: {data}, 
+    props: {
+      data, session
+    }
   }
 }
